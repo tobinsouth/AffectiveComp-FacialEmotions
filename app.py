933bae0e1ef4b3a5b3ab46c3c3347c01486dbeab
mikehaojiang <mike.jiang@bluewin.ch>
2021-11-24 14:49:29 -0500

    enabled pure client-side video stream. Aiming to interface Client with Server using SocketIO. Have decided on an architecture consisting of two main routes, each dedicated to a person. Each person sees an unaltered self, an altered self and the altered other. The unaltered self is prely client-side, the altered self goes through the server and will also be sent to the other person's route. Essentially the altered feed is sent twice - once to oneself and once to the other person.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from imutils.video import VideoStream
 import imutils
 import cv2
 import os
+# import socketio
 
 app = Flask(__name__)
 CORS(app, resources=r'/api/*')
@@ -44,7 +45,35 @@ def emotion_swap():
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
+
+# @socketio.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
+
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    ## converting RGB to BGR, as opencv standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # Process the image frame
+    frame = imutils.resize(frame, width=700)
+    frame = cv2.flip(frame, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+
+    # emit the frame back
+    emit('response_back', stringData)
+
+
 if __name__ == '__main__':
-    # app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
